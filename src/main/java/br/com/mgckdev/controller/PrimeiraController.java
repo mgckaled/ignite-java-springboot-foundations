d686673a8b864d129aa9b2b6da16830c91a91a20
mgckaled <marcelgckaled@gmail.com>
2023-10-24 15:10:59 -0300

    feat: add path params to first controller

--- src/main/java/br/com/mgckdev/controller/PrimeiraController.java
+++ src/main/java/br/com/mgckdev/controller/PrimeiraController.java
@@ -1,15 +1,30 @@
 package br.com.mgckdev.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiraController")
 public class PrimeiraController {
 
-  @GetMapping("/primeiroMetodo")
-  public String primeiroMetodo() {
-    return "Parabéns! Você criou a sua primeira rota API REST";
+  @GetMapping("/primeiroMetodo/{id}")
+  public String primeiroMetodo(@PathVariable String id) {
+    return "O parêmetro é " + id;
   }
+
+  @GetMapping("metodoComQueryParams")
+  public String metodoComQueryParams(@RequestParam String id) {
+    return "O parêmetro com metodoComQueryParams é " + id;
+  }
+
+  @GetMapping("metodoComTodasQueryParams")
+  public String x(@RequestParam Map<String, String> allParams) {
+    return "O parêmetro com metodoComTodasQueryParams é " + allParams.entrySet();
+  }
+
 }
